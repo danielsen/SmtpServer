c15821f933d31f51e0b271ecbdb0de6e5e329f4d
danielsen <dnielsen@fastmail.fm>
2019-01-08 21:51:16 -0600

    Refactor to net core for tests.

---
+++ Src/SmtpServer.Tests/Mocks/MockMessageStore.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using SmtpServer.Protocol;
+using SmtpServer.Storage;
+
+namespace SmtpServer.Tests.Mocks
+{
+    public class MockMessageStore : MessageStore
+    {
+        public MockMessageStore()
+        {
+            Messages = new List<IMessageTransaction>();
+        }
+
+        public override Task<SmtpResponse> SaveAsync(ISessionContext context, IMessageTransaction transaction, CancellationToken cancellationToken)
+        {
+            Messages.Add(transaction);
+
+            return Task.FromResult(SmtpResponse.Ok);
+        }
+
+        public List<IMessageTransaction> Messages { get; }
+    }
+}
